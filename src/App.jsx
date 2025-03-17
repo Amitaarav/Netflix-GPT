645d74229ea556a26b16e55b40e0c3f577fe7724
[Amit Kumar] <[aaravamt555@gmail.com]>
2025-03-17 19:06:29 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <div className='text-3xl text-center mt-10 bg-green-500'>
+      Namaste Everyone, Let's build netflixGPT together!
+    </div>
+  )
+}
+
+export default App
